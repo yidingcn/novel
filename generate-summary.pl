1ff001fd6f252acf8d7d80c8e01a996818b3f93d
Karata <vabc3h@gmail.com>
2018-01-06 23:41:32 +0800

    Script refinement

--- generate-summary.pl
+++ generate-summary.pl
@@ -1,8 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -CS
 use 5.012;
+use utf8;
 
-for(sort {chapter_index($a) <=> chapter_index($b)} <*.md>) {
-    next if /README|SUMMARY/;
+for(sort
+    {chapter_index($a) <=> chapter_index($b)}
+    map {utf8::decode($_); $_} <*.md>) {
+    next if /README/;
     /(.*).md/ && say "- [$1](./$_)";
 }
 
@@ -10,9 +13,6 @@ sub chapter_index { $_[0] =~ /^第(.*)章/ ? chn2num($1) : -1 }
 
 sub chn2num {
     $_ = shift;
-    return 10 if $_ eq '十';
-    s/一/1/g;s/二/2/g;s/三/3/g;
-    s/四/4/g;s/五/5/g;s/六/6/g;
-    s/七/7/g;s/八/8/g;s/九/9/g;
-    s/十$/0/;s/^十/1/;s/十//r;
+    tr/一二三四五六七八九/123456789/;
+    s/^(?=十)/1/;s/十$/0/;tr/十//dr;
 }
